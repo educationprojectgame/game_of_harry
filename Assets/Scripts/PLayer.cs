ff2d58090f1f4aa3e1f468b958149363d4c4f26f
NazarovReal <nazarovreal5@gmail.com>
2025-05-14 23:16:55 +0500

    bulletUpdate

--- Assets/Scripts/PLayer.cs
+++ Assets/Scripts/PLayer.cs
@@ -109,6 +109,11 @@ public class Player : MonoBehaviour
 
     private void Shoot()
     {
+        if (shootPos == null)
+        {
+            Debug.LogWarning("Позиция стрельбы (shootPos) была уничтожена или не назначена!");
+            return;
+        }
         Instantiate(bullet, shootPos.position, shootPos.rotation);
     }
 }
