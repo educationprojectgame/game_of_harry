a89073fdf06c81d7041be409a425369cbb809018
NazarovReal <nazarovreal5@gmail.com>
2025-06-01 14:43:54 +0500

    castle

---
+++ Assets/Scripts/GameManager4.cs
@@ -0,0 +1,93 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChessManager4 : MonoBehaviour
+{
+    public GameObject knightPrefab;
+    public GameObject enemyPrefab;
+    public GameObject keyPrefab;
+
+    private GameObject knight;
+    private Vector2Int playerPosition = new Vector2Int(2, 2);
+    private float cellSize = 1f;
+    private Vector2 boardOrigin = new Vector2(-3.5f, -3.5f);
+
+    private Vector2Int keyPosition = new Vector2Int(7, 7);
+    private Vector2Int[] enemyPositions = new Vector2Int[]
+    {
+        new Vector2Int(2, 4),
+        new Vector2Int(2, 7),
+        new Vector2Int(3, 2),
+        new Vector2Int(6, 3),
+        new Vector2Int(1, 5),
+        new Vector2Int(4, 4)
+    };
+
+    void Start()
+    {
+        knight = Instantiate(knightPrefab, PositionToWorld(playerPosition), Quaternion.identity);
+        Instantiate(keyPrefab, PositionToWorld(keyPosition), Quaternion.identity);
+
+        foreach (var pos in enemyPositions)
+        {
+            Instantiate(enemyPrefab, PositionToWorld(pos), Quaternion.identity);
+        }
+    }
+
+    void Update()
+    {
+        Vector2Int[] moves = {
+            new Vector2Int(1, 2), new Vector2Int(2, 1),
+            new Vector2Int(-1, 2), new Vector2Int(-2, 1),
+            new Vector2Int(1, -2), new Vector2Int(2, -1),
+            new Vector2Int(-1, -2), new Vector2Int(-2, -1)
+        };
+
+        for (int i = 0; i < moves.Length; i++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + i))
+            {
+                Vector2Int targetPos = playerPosition + moves[i];
+                if (IsInsideBoard(targetPos))
+                {
+                    playerPosition = targetPos;
+                    knight.transform.position = PositionToWorld(playerPosition);
+                    CheckCollision();
+                }
+            }
+        }
+    }
+
+    Vector3 PositionToWorld(Vector2Int pos)
+    {
+        return new Vector3(boardOrigin.x + pos.x * cellSize, boardOrigin.y + pos.y * cellSize, -1);
+    }
+
+    bool IsInsideBoard(Vector2Int pos)
+    {
+        return pos.x >= 0 && pos.x < 8 && pos.y >= 0 && pos.y < 8;
+    }
+
+    void CheckCollision()
+    {
+        if (playerPosition == keyPosition)
+        {
+            Debug.Log("Победа! Гарри нашёл ключ!");
+
+            // Устанавливаем координаты возвращения
+            TeleportData.returnPosition = new Vector2(-112.7f, 79.9f);
+
+            // Загружаем сцену карты
+            SceneManager.LoadScene("Castle"); // Проверь, что сцена есть в Build Settings
+        }
+
+        foreach (var enemy in enemyPositions)
+        {
+            if (playerPosition == enemy)
+            {
+                Debug.Log("Гарри пойман врагом! Игра окончена.");
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+}
