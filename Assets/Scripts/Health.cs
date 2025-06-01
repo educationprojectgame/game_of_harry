4467b3d034b036d4b4d221ad9c97e30ae9249fcb
Denis Brevnov <denisbrevnov2006@gmail.com>
2025-06-01 14:24:27 +0500

    fix hp for m 2

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -32,7 +32,7 @@ public class Health : MonoBehaviour
         currentHealth -= damage;
         Debug.Log($"Boss {bossId} took {damage} damage, current health: {currentHealth}");
 
-        Bar.fillAmount = currentHealth / 200;
+        Bar.fillAmount = currentHealth / maxHealth;
         if (maxHealth <= 0 || currentHealth <= 0)
         {
             Die();
